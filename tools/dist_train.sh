dfd2fda4260b3f1f624b31f7d209bc18653535f3
darrenwang <darrenwang@didiglobal.com>
2022-05-25 11:47:13 +0800

    fix bug

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,3 +1,4 @@
+GPUS=$1
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
@@ -11,4 +12,4 @@ python3 -m torch.distributed.launch \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
     $(dirname "$0")/train.py \
-    --launcher pytorch ${@:1}
+    --launcher pytorch ${@:2}
