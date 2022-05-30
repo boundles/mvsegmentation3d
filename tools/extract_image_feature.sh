df700ff82b8e083334db3bb959d3b456ddc73386
darrenwang <darrenwang@didiglobal.com>
2022-05-30 16:53:19 +0800

    modify image feature extractor

--- tools/extract_image_feature.sh
+++ tools/extract_image_feature.sh
@@ -4,7 +4,8 @@ set -x
 
 DATA_DIR=$1
 WORK_DIR=$2
-CAMERA_ID=$3
-SPLIT=$4
+SPLIT=$3
+PATHNAMES_FILE=$4
 
-python3 -u $(dirname $0)/extract_image_feature.py $DATA_DIR $WORK_DIR $CAMERA_ID $SPLIT
+
+python3 -u $(dirname $0)/extract_image_feature.py $DATA_DIR $WORK_DIR $SPLIT $PATHNAMES_FILE
