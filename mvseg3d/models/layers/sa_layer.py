b8616f83672533eb6bad776a8770bca6af709207
darrenwang <darrenwang@didiglobal.com>
2022-07-05 17:14:47 +0800

    use large ks

--- mvseg3d/models/layers/sa_layer.py
+++ mvseg3d/models/layers/sa_layer.py
@@ -8,7 +8,7 @@ from mvseg3d.utils.spconv_utils import replace_feature
 class SALayer(nn.Module):
     def __init__(self, planes, indice_key):
         super(SALayer, self).__init__()
-        self.conv = spconv.SubMConv3d(planes, 1, 3, padding=1, bias=False, indice_key=indice_key)
+        self.conv = spconv.SubMConv3d(planes, 1, 7, padding=3, bias=False, indice_key=indice_key + 'sa')
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
