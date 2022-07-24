d0c2499aa9aedaadecb3e476e0da840020c8f4b7
darrenwang <darrenwang@didiglobal.com>
2022-07-24 21:01:23 +0800

    add fps

--- mvseg3d/models/layers/context_layer.py
+++ mvseg3d/models/layers/context_layer.py
@@ -1,4 +1,49 @@
+import torch
 import torch.nn as nn
+
+
+def square_distance(src, dst):
+    """
+    Calculate Euclid distance between each two points.
+    src^T * dst = xn * xm + yn * ym + zn * zm；
+    sum(src^2, dim=-1) = xn*xn + yn*yn + zn*zn;
+    sum(dst^2, dim=-1) = xm*xm + ym*ym + zm*zm;
+    dist = (xn - xm)^2 + (yn - ym)^2 + (zn - zm)^2
+         = sum(src**2,dim=-1)+sum(dst**2,dim=-1)-2*src^T*dst
+    Input:
+        src: source points, [B, N, C]
+        dst: target points, [B, M, C]
+    Output:
+        dist: per-point square distance, [B, N, M]
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist
+
+def farthest_point_sample(xyz, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        distance = torch.min(distance, dist)
+        farthest = torch.max(distance, -1)[1]
+    return centroids
 
 class SelfAttention(nn.Module):
     def __init__(self, embed_dim, num_heads):
@@ -40,6 +85,7 @@ class ContextLayer(nn.Module):
     def __init__(self, planes):
         super(ContextLayer, self).__init__()
 
+        self.n_sample = 1000
         self.attn = SelfAttention(planes, 4)
 
     def forward(self, x):
@@ -51,6 +97,9 @@ class ContextLayer(nn.Module):
         """
         indices = x.indices.long()
         for i in range(x.batch_size):
-            features = x.features[indices[:, 0] == i].unsqueeze(0)
-            features = self.attn(features).squeeze(0)
+            i_indices = indices[indices[:, 0] == i][1:].unsqueeze(0)
+            sample_indices = farthest_point_sample(i_indices, self.n_sample).squeeze(0)
+            sample_features = x.features[sample_indices].unsqueeze(0)
+            sample_features = self.attn(sample_features).squeeze(0)
+            print(sample_features.shape)
         return x
