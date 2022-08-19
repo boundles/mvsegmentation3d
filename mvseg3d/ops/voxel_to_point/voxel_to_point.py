b21c1dfb3684d3546a00f0ded9954bf0f64dac7e
darrenwang <darrenwang@didiglobal.com>
2022-08-19 14:29:54 +0800

    rename

--- mvseg3d/ops/voxel_to_point/voxel_to_point.py
+++ mvseg3d/ops/voxel_to_point/voxel_to_point.py
@@ -11,10 +11,10 @@ class VoxelToPoint(object):
             point_features: (N, C)
         """
         res_feature_shape = (coords.shape[0], feats.shape[-1])
-        res = torch.zeros(res_feature_shape, dtype=feats.dtype, device=feats.device)
+        res_feature = torch.zeros(res_feature_shape, dtype=feats.dtype, device=feats.device)
         coords_valid = torch.nonzero(coords != -1).view(-1)
-        res[coords_valid] = feats[coords[coords_valid]]
-        return res
+        res_feature[coords_valid] = feats[coords[coords_valid]]
+        return res_feature
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
