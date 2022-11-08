8f7c638afd7bcd0344ae7831f712dd609ee686ae
darrenwang <darrenwang@didiglobal.com>
2022-11-08 15:55:41 +0800

    output decoded features

--- mvseg3d/models/backbones/pointtransformer.py
+++ mvseg3d/models/backbones/pointtransformer.py
@@ -5,7 +5,7 @@ from mvseg3d.models.layers import PointTransformerBlock, TransitionDown, Transit
 
 
 class PointTransformer(nn.Module):
-    def __init__(self, in_channel, num_class):
+    def __init__(self, in_channel):
         super(PointTransformer, self).__init__()
         block = PointTransformerBlock
         num_block = [2, 3, 4, 6, 3]
@@ -28,8 +28,9 @@ class PointTransformer(nn.Module):
         self.dec3 = self._make_dec(block, planes[2], 2, share_planes, nsample=nsample[2])  # fusion p4 and p3
         self.dec2 = self._make_dec(block, planes[1], 2, share_planes, nsample=nsample[1])  # fusion p3 and p2
         self.dec1 = self._make_dec(block, planes[0], 2, share_planes, nsample=nsample[0])  # fusion p2 and p1
-        self.cls = nn.Sequential(nn.Linear(planes[0], planes[0]), nn.BatchNorm1d(planes[0]), nn.ReLU(inplace=True),
-                                 nn.Linear(planes[0], num_class))
+        self.out = nn.Sequential(nn.Linear(planes[0], planes[0]),
+                                 nn.BatchNorm1d(planes[0]),
+                                 nn.ReLU(inplace=True))
 
     def _make_enc(self, block, planes, blocks, share_planes=8, stride=1, nsample=16, num_sector=1):
         layers = [TransitionDown(self.in_planes, planes * block.expansion, stride, nsample, num_sector)]
@@ -58,5 +59,5 @@ class PointTransformer(nn.Module):
         x3 = self.dec3[1:]([p3, self.dec3[0]([p3, x3, o3], [p4, x4, o4]), o3])[1]
         x2 = self.dec2[1:]([p2, self.dec2[0]([p2, x2, o2], [p3, x3, o3]), o2])[1]
         x1 = self.dec1[1:]([p1, self.dec1[0]([p1, x1, o1], [p2, x2, o2]), o1])[1]
-        x = self.cls(x1)
+        x = self.out(x1)
         return x
--- tests/test_point_transformer.py
+++ tests/test_point_transformer.py
@@ -4,9 +4,11 @@ from mvseg3d.models.backbones import PointTransformer
 
 
 if __name__ == '__main__':
-    point_transformer = PointTransformer(16, 8)
+    model = PointTransformer(16, 8)
+    model = model.cuda()
 
     points = torch.randn((1800, 3)).cuda()
     features = torch.randn((1800, 16)).cuda()
-    offsets = torch.IntTensor([900, 900]).cuda()
-    out = point_transformer((points, features, offsets))
+    offsets = torch.IntTensor([900, 1800]).cuda()
+    out = model((points, features, offsets))
+    print(out.shape)
