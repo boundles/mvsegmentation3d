0b3e6139b9c6357f372fd0083507cb554f2a3854
darrenwang <darrenwang@didiglobal.com>
2022-11-25 17:23:25 +0800

    rename drop info

--- tests/test_window_attention.py
+++ tests/test_window_attention.py
@@ -4,16 +4,17 @@ from mvseg3d.models.layers import SparseWindowPartitionLayer, WindowAttention
 
 
 if __name__ == '__main__':
-    drop_info = {
-        0: {'max_tokens': 30, 'drop_range': (0, 30)},
-        1: {'max_tokens': 60, 'drop_range': (30, 60)},
-        2: {'max_tokens': 100, 'drop_range': (60, 100000)}
+    batching_info = {
+        0: {'max_tokens': 60, 'batching_range': (0, 60)},
+        1: {'max_tokens': 120, 'batching_range': (60, 120)},
+        2: {'max_tokens': 180, 'batching_range': (120, 180)},
+        3: {'max_tokens': 400, 'batching_range': (180, 100000)}
     }
     window_shape = (10, 10, 10)
     sparse_shape = (400, 400, 20)
     normalize_pos = False
     pos_temperature = 10000
-    window_partition = SparseWindowPartitionLayer(drop_info, window_shape, sparse_shape)
+    window_partition = SparseWindowPartitionLayer(batching_info, window_shape, sparse_shape)
 
     voxel_features = torch.randn((4, 24)).cuda()
     voxel_coords = torch.zeros((4, 4)).cuda()
