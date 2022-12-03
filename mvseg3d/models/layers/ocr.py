bdaa615774b164616726aa9138f19afe5d47514e
darrenwang <darrenwang@didiglobal.com>
2022-12-04 00:16:53 +0800

    modify ocr

--- mvseg3d/models/layers/ocr.py
+++ mvseg3d/models/layers/ocr.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import spconv.pytorch as spconv
+
 from mvseg3d.utils.spconv_utils import replace_feature
 
 
@@ -37,19 +39,25 @@ class OCRLayer(nn.Module):
         super(OCRLayer, self).__init__()
 
         self.scale = scale
+        self.transform_input = self.conv_input = spconv.SparseSequential(
+            spconv.SubMConv3d(in_channels, in_channels, 3, padding=1, bias=False),
+            nn.BatchNorm1d(in_channels),
+            nn.ReLU(inplace=True)
+        )
         self.spatial_gather_module = SpatialGatherModule(self.scale)
         self.attn = nn.MultiheadAttention(in_channels, nhead, dropout=attn_drop)
         self.bottleneck = nn.Linear(in_channels * 2, in_channels, bias=False)
 
     def forward(self, inputs, probs, batch_size):
-        feats = inputs.features.clone()
+        inputs = self.transform_input(inputs)
+        feats = inputs.features
         batch_indices = inputs.indices[:, 0]
-        ocr_context = self.spatial_gather_module(feats, probs, batch_size, batch_indices)
+        ocr_context = self.spatial_gather_module(inputs.features, probs, batch_size, batch_indices)
         for i in range(batch_size):
-            feat = feats[batch_indices == i]
-            context = ocr_context[i]
-            attn_out, attn_weights = self.attn(feat, context, context)
-            feats[batch_indices == i] = attn_out
+            query_feat = inputs.features[batch_indices == i]
+            value_feat = key_feat = ocr_context[i]
+            attn_out, attn_weights = self.attn(query_feat, key_feat, value_feat)
+            inputs.features[batch_indices == i] = attn_out
         feats = torch.cat([inputs.features, feats], dim=1)
         feats = self.bottleneck(feats)
         inputs = replace_feature(inputs, feats)
