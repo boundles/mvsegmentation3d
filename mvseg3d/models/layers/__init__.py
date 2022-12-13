c1ebe7322b9ff2ffea763b3c4c6855e93fa17472
darrenwang <darrenwang@didiglobal.com>
2022-12-13 11:25:36 +0800

    add dropout for deepfusion

--- mvseg3d/models/layers/__init__.py
+++ mvseg3d/models/layers/__init__.py
@@ -2,5 +2,6 @@ from .se_layer import FlattenSELayer
 from .sa_layer import SALayer
 from .point_transformer_layer import SparseWindowPartitionLayer, WindowAttention
 from .ocr import OCRLayer
+from .deep_fusion import DeepFusionBlock
 
-__all__ = ['FlattenSELayer', 'SALayer', 'SparseWindowPartitionLayer', 'WindowAttention', 'OCRLayer']
+__all__ = ['FlattenSELayer', 'SALayer', 'SparseWindowPartitionLayer', 'WindowAttention', 'OCRLayer', 'DeepFusionBlock']
--- mvseg3d/models/segmentors/segformer.py
+++ mvseg3d/models/segmentors/segformer.py
@@ -2,51 +2,11 @@ from collections import OrderedDict
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-import numpy as np
 
 from mvseg3d.models.voxel_encoders import VFE
 from mvseg3d.models.backbones import PointTransformer
-from mvseg3d.models.layers import FlattenSELayer
-from mvseg3d.ops import knn_query, voxel_to_point
-
-
-class DeepFusionBlock(nn.Module):
-    def __init__(self, lidar_channel, image_channel, hidden_channel, n_neighbors, attn_pdrop=0.5):
-        super(DeepFusionBlock, self).__init__()
-
-        self.lidar_channel = lidar_channel
-        self.image_channel = image_channel
-        self.n_neighbors = n_neighbors
-
-        self.q_embedding = nn.Linear(lidar_channel, hidden_channel)
-        self.k_embedding = nn.Linear(image_channel, hidden_channel)
-        self.v_embedding = nn.Linear(image_channel, hidden_channel)
-
-        self.attn_dropout = nn.Dropout(attn_pdrop)
-
-        self.c_proj = nn.Linear(hidden_channel, image_channel)
-
-    def forward(self, points, point_id_offset, lidar_features, image_features):
-        q = self.q_embedding(lidar_features)
-        k = self.k_embedding(image_features)
-        v = self.v_embedding(image_features)
-
-        knn_ids, _ = knn_query(self.n_neighbors, points, points, point_id_offset, point_id_offset)
-        k = k[knn_ids.long()]
-        attn = torch.einsum('nc,nkc->nk', q, k) / np.sqrt(q.shape[-1])
-
-        invalid_mask = (torch.sum(image_features, dim=1) == 0)
-        invalid_mask = invalid_mask[knn_ids.long()]
-        attn[invalid_mask] = float('-inf')
-        attn = F.softmax(attn, dim=-1)
-        attn = torch.nan_to_num(attn)
-
-        v = v[knn_ids.long()]
-        y = torch.einsum('nk,nkc->nc', attn, v)
-        y = self.c_proj(y)
-        return y
+from mvseg3d.models.layers import FlattenSELayer, DeepFusionBlock
+from mvseg3d.ops import voxel_to_point
 
 
 class Segformer(nn.Module):
