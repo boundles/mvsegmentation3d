274badd6e4b2d01b531bbc5fd34431a348fbe280
darrenwang <darrenwang@didiglobal.com>
2022-12-23 17:51:54 +0800

    add tta

---
+++ mvseg3d/datasets/transforms/test_time_aug.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from mvseg3d.datasets.transforms import transform_utils
+
+
+class MultiScaleFlipAug(object):
+    """Test-time augmentation with multiple scales and flipping."""
+    def __init__(self, scales=None, angles=None, flip_x=False, flip_y=False):
+        self.scales = scales
+        self.angles = angles
+        self.flip_x = [True, False] if flip_x else [False]
+        self.flip_y = [True, False] if flip_y else [False]
+
+    def __call__(self, data_dict):
+        """Call function to apply test time augment transforms on results."""
+        aug_data_list = []
+        for scale in self.scales:
+            for angle in self.angles:
+                for flip_x in self.flip_x:
+                    for flip_y in self.flip_y:
+                        new_data_dict = data_dict.copy()
+                        new_data_dict['points'][:, :3] *= scale
+                        new_data_dict['points'] = transform_utils.rotate_points_along_z(new_data_dict['points'][np.newaxis, :, :], np.array([angle]))[0]
+                        if flip_x:
+                            new_data_dict['points'][:, 1] = -new_data_dict['points'][:, 1]
+                        if flip_y:
+                            new_data_dict['points'][:, 0] = -new_data_dict['points'][:, 0]
+                        aug_data_list.append(new_data_dict)
+        return aug_data_list
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(scales={self.scales}, '
+        repr_str += f'(angles={self.angles}, '
+        repr_str += f'(flip_x={self.flip_x}, '
+        repr_str += f'(flip_y={self.flip_y}'
+        return repr_str
